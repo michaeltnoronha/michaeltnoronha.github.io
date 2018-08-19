baa015d16f3bd2e81c80a074f8df7c7e0d03eac0
Michael Noronha <michaeltnoronha@gmail.com>
2018-08-18 17:11:01 -0700

    rebuilding site Sat Aug 18 17:11:01 PDT 2018

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Resume location, relative to publicdir
+# resume="../../resume/resume.pdf"
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project.
+hugo
+
+# Update resume.pdf
+# cp $resume .
+
+# Add changes to git.
+git add docs
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
